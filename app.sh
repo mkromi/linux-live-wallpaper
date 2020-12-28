0c4e1fa548bbe7f3b446f21b841c579f0c23fb34
Mohiuddin Khan <mohiuddinkhanromi@gmail.com>
2020-12-28 17:53:07 +0600

    File location changed

--- app.sh
+++ app.sh
@@ -1,5 +1,5 @@
 Xaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
 Yaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
 
-wget https://source.unsplash.com/random/${Xaxis}x${Yaxis} -O /usr/share/wallpapers/deepin/desktop.jpg --no-check-certificate
-gsettings set org.gnome.desktop.background picture-uri file:////usr/share/wallpapers/deepin/desktop.jpg
+wget https://source.unsplash.com/random/${Xaxis}x${Yaxis} -O /tmp/wallpaper.png --no-check-certificate
+gsettings set org.gnome.desktop.background picture-uri file:////tmp/wallpaper.png
