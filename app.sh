4c87fedb1199f334bd73c131cbaebb9677de62d2
mkromi <30689789+mkromi@users.noreply.github.com>
2021-01-23 16:21:06 +0600

    Issue fixed for dual monitor with multiple screen resolution image fetch.

--- app.sh
+++ app.sh
@@ -5,11 +5,14 @@ export DISPLAY=:0
 Xaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
 Yaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
 
+splitXaxis=(${Xaxis// / })
+splitYaxis=(${Yaxis// / })
+
 tags=$(jq -r '.tags' $PWD/linux-live-wallpaper/data.json)
 
 IFS=', ' read -r -a array <<< "$tags"
 size=${#array[@]}
 index=$(($RANDOM % $size))
 
-wget https://source.unsplash.com/${Xaxis}x${Yaxis}?${array[$index]} -O /tmp/wallpaper.png --no-check-certificate
+wget https://source.unsplash.com/${splitXaxis[0]}x${splitYaxis[0]}?${array[$index]} -O /tmp/wallpaper.png --no-check-certificate
 gsettings set org.gnome.desktop.background picture-uri file:////tmp/wallpaper.png
